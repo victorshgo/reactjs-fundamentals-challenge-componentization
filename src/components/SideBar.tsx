258c5f449728cb8d2ed00e269a407011ab15c53d
Victor Hugo <victorshgo@outlook.com>
2022-06-16 20:29:33 -0300

    Update SideBar.tsx

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,33 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+import { GenreResponseProps } from "../@types/genre.model";
+
+import "../styles/sidebar.scss";
+
+interface SideBarProps {
+  id: number;
+  genres: GenreResponseProps[];
+  handleClickButton: Function;
+}
+
+export function SideBar({ id, genres, handleClickButton }: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={id === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
